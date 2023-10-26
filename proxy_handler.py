3f6ec10afcb9e723ae9aa7dbdfca42ad831afd1d
Kaweendra <salitha.wijerathna@gmail.com>
2023-10-26 16:14:18 +0530

    Update proxy_handler optimize

--- proxy_handler.py
+++ proxy_handler.py
@@ -1,77 +1,71 @@
 import socket
 import threading
-import select
-from proxy_config import ProxyConfig
 
 class ProxyHandler:
     def __init__(self):
         self.config = ProxyConfig()
 
     def handle_client(self, connection):
-        version, nmethods = connection.recv(2)
-        methods = self.get_available_methods(nmethods, connection)
+        with connection:
+            version, nmethods = connection.recv(2)
+            methods = self.get_available_methods(nmethods, connection)
 
-        if 2 not in set(methods):
-            connection.close()
-            return
+            if 2 not in set(methods):
+                return
 
-        connection.sendall(bytes([self.config.SOCKS_VERSION, 2]))
+            connection.sendall(bytes([self.config.SOCKS_VERSION, 2]))
 
-        if not self.verify_credentials(connection):
-            return
+            if not self.verify_credentials(connection):
+                return
 
-        version, cmd, _, address_type = connection.recv(4)
+            version, cmd, _, address_type = connection.recv(4)
 
-        if address_type == 1:  
-            address = socket.inet_ntoa(connection.recv(4))
-        elif address_type == 3:  
-            domain_length = connection.recv(1)[0]
-            address = connection.recv(domain_length)
-            address = socket.gethostbyname(address)
+            if address_type == 1:
+                address = socket.inet_ntoa(connection.recv(4))
+            elif address_type == 3:
+                domain_length = connection.recv(1)[0]
+                address = connection.recv(domain_length)
+                address = socket.gethostbyname(address)
 
-        port = int.from_bytes(connection.recv(2), 'big', signed=False)
+            port = int.from_bytes(connection.recv(2), 'big', signed=False)
 
-        try:
-            if cmd == 1:  
-                remote = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-                remote.connect((address, port))
-                bind_address = remote.getsockname()
-                print(f"* Connected to {address} {port}")
-            else:
-                connection.close()
+            try:
+                if cmd == 1:
+                    remote = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                    remote.connect((address, port))
+                    bind_address = remote.getsockname()
+                    print(f"* Connected to {address} {port}")
+                else:
+                    connection.close()
 
-            addr = int.from_bytes(socket.inet_aton(bind_address[0]), 'big', signed=False)
-            port = bind_address[1]
+                addr = int.from_bytes(socket.inet_aton(bind_address[0]), 'big', signed=False)
+                port = bind_address[1]
 
-            reply = b''.join([
-                self.config.SOCKS_VERSION.to_bytes(1, 'big'),
-                int(0).to_bytes(1, 'big'),
-                int(0).to_bytes(1, 'big'),
-                int(1).to_bytes(1, 'big'),
-                addr.to_bytes(4, 'big'),
-                port.to_bytes(2, 'big')
-            ])
-        except Exception as e:
-            reply = self.generate_failed_reply(address_type, 5)
+                reply = b''.join([
+                    self.config.SOCKS_VERSION.to_bytes(1, 'big'),
+                    int(0).to_bytes(1, 'big'),
+                    int(0).to_bytes(1, 'big'),
+                    int(1).to_bytes(1, 'big'),
+                    addr.to_bytes(4, 'big'),
+                    port.to_bytes(2, 'big')
+                ])
+            except Exception as e:
+                reply = self.generate_failed_reply(address_type, 5)
 
-        connection.sendall(reply)
+            connection.sendall(reply)
 
-        if reply[1] == 0 and cmd == 1:
-            self.exchange_loop(connection, remote)
-
-        connection.close()
-
-    # ... (other methods like exchange_loop, generate_failed_reply, verify_credentials, get_available_methods)
+            if reply[1] == 0 and cmd == 1:
+                self.exchange_loop(connection, remote)
 
     def run(self):
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.bind((self.config.HOST, self.config.PORT))
-        s.listen()
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.bind((self.config.HOST, self.config.PORT))
+            s.listen()
 
-        print(f"* Socks5 proxy server is running on {self.config.HOST}:{self.config.PORT}")
+            print(f"* Socks5 proxy server is running on {self.config.HOST}:{self.config.PORT}")
 
-        while True:
-            conn, addr = s.accept()
-            print(f"* new connection from {addr}")
-            t = threading.Thread(target=self.handle_client, args=(conn,))
-            t.start()
+            while True:
+                conn, addr = s.accept()
+                print(f"* New connection from {addr}")
+                t = threading.Thread(target=self.handle_client, args=(conn,))
+                t.start()
